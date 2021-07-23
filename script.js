921c9dd32a2438f2b6cac8285bcd59d715a74b0e
p <sudogrit0@gmail.com>
2021-07-23 15:03:03 +0700

    add sell price

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const app = new Vue({
     el: '.btc-badge',
     data: {
         btcUSD: '0.00',
+	sell: '0.00',
         percentChange: '0',
+        sellChange: '0',
         upDown: 'up',
     },
     mounted() {
@@ -22,17 +24,15 @@ const app = new Vue({
             }
         },
         getInfo() {
-            //   axios.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH&tsyms=USD&e=Coinbase')
             axios.get('https://apicheckprice.huasengheng.com/api/values/getprice/')
                 //   .then(function (response) {
                 .then(response => {
-                    const { GoldType, Buy, Sell, BuyChange, TimeUpdate } = response.data[0]
+                    const { GoldType, Buy, Sell, BuyChange, SellChange, TimeUpdate } = response.data[0]
                     console.log(`${response.data[0].BuyChange}`)
                     const open = Buy
                     this.getChange(BuyChange)
-                        // const newPrice = response.data.RAW.BTC.USD.PRICE
-                    console.log(open);
-                    //   this.getPercentChange(newPrice, open);
+		    this.sell = Sell
+                    this.sellChange = SellChange
                     this.btcUSD = Buy
                 });
         }
